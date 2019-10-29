f420afacff9cab1d4c4d6c4b21fbd541a2c9de74
ISHAN&ARJUN <ISHAN&ARJUN@LAPTOP-GEI4GPBO>
2019-10-29 11:30:18 -0400

    Added Demoloop1.java

---
+++ TestProject/src/Javatutorial/Demoloop1.java
@@ -0,0 +1,17 @@
+package Javatutorial;
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+
+
+public class Demoloop1 {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner (System.in);
+		int num= scan.nextInt();
+		for(int i=0;i<=10;i++)
+		{System.out.printf("%d*%d=%d%n",num,i,num*i);
+}
+}
+}
