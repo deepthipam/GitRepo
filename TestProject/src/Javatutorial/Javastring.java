ea444078345352297b6833ff500d9b82931426a6
ISHAN&ARJUN <ISHAN&ARJUN@LAPTOP-GEI4GPBO>
2019-11-08 12:03:46 -0500

    added javastring program

---
+++ TestProject/src/Javatutorial/Javastring.java
@@ -0,0 +1,22 @@
+package Javatutorial;
+
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Javastring {
+
+			public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        String S = in.next();
+	        int start = in.nextInt();
+	        int end = in.nextInt();
+
+	        System.out.println(S.substring(start, end));
+	    
+
+	}
+
+}
