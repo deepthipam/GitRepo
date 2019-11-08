2ac5d9fbd7516a952861f829eb660461c1aa3fc8
ISHAN&ARJUN <ISHAN&ARJUN@LAPTOP-GEI4GPBO>
2019-11-08 11:43:09 -0500

    added demostrformat  to git

---
+++ TestProject/src/Javatutorial/Demostrformat.java
@@ -0,0 +1,27 @@
+package Javatutorial;
+
+import java.io.*;
+import java.util.*;
+
+public class Demostrformat { 
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+         int sumOfStrings = A.length() + B.length();
+        System.out.println(sumOfStrings);
+        if (A.compareTo(B) == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " +
+                B.substring(0, 1).toUpperCase() + B.substring(1));
+
+        
+    }
+}
